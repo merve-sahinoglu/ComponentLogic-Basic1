2a4c19a62edcb37f820c38e7ea020bbc408d760d
Merve Şahinoğlu <artemiswolfs@gmail.com>
2023-09-19 14:09:34 +0300

    alert ana ekrandan çağrılması sağülandı.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import ListGroup from "./component/ListGroup";
+import Alert from "./component/Alert";
 
 function App() {
+  const [alertVisible,setAlertVisibility]=useState(false);
   let items = ["New York", "San Francisco"];
 
   const onSelectItem = (item: string) => {
@@ -9,6 +12,7 @@ function App() {
 
   return (
     <div>
+      {alertVisible && <Alert onClose={() =>setAlertVisibility(true)}>My Alert</Alert>}
       <ListGroup items={items} heading="Slm" onSelectItem={onSelectItem} />
     </div>
   );
