6ee58948f1a34483ed5573d6895e8a03705bcf9c
Merve Şahinoğlu <artemiswolfs@gmail.com>
2023-09-25 15:48:35 +0300

    zodResolver eklendı.Form.tsx de kullanıldı.

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -1,140 +1,64 @@
-import { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Age must be at least 18." }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
-  //   console.log(formState.errors);
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.table(data);
 
-  //#region old(useState example)
-  //   const [person, setPerson] = useState({
-  //     name: "",
-  //     age: 0,
-  //   });
-  //   const handleSubmit = (event: FormEvent) => {
-  //     event.preventDefault();
-  //     console.table(person);
-  //   };
-  //#endregion
-
-  //#region old(useRef example)
-  //   const nameRef = useRef<HTMLInputElement>(null);
-  //   const ageRef = useRef<HTMLInputElement>(null);
-  //   const person = {
-  //     name: "",
-  //     age: 0,
-  //   };
-
-  //   const handleSubmit = (event: FormEvent) => {
-  //     event.preventDefault();
-  //     if (nameRef.current) {
-  //       person.name = nameRef.current.value;
-  //     }
-  //     if (ageRef.current) {
-  //       person.age = parseInt(ageRef.current.value);
-  //     }
-
-  //     console.table(person);
-  //   };
-  //#endregion
-
   return (
-    //#region  old(useRef example)
-    // <form onSubmit={handleSubmit}>
-    //   <div className="mb-3">
-    //     <label htmlFor="name" className="form-label">
-    //       Name
-    //     </label>
-    //     <input id="name" ref={nameRef} type="text" className="form-control" />
-    //   </div>
-    //   <div className="mb-3">
-    //     <label htmlFor="age" className="form-label">
-    //       Age
-    //     </label>
-    //     <input id="age" ref={ageRef} type="number" className="form-control" />
-    //   </div>
-    //   <button className="btn btn-primary" type="submit">
-    //     Submit
-    //   </button>
-    // </form>
-    //#endregion
-
-    //#region old(useState example)
-    // <form onSubmit={handleSubmit}>
-    //   <div className="mb-3">
-    //     <label htmlFor="name" className="form-label">
-    //       Name
-    //     </label>
-    //     <input
-    //       onChange={(event) =>
-    //         setPerson({ ...person, name: event.target.value })
-    //       }
-    //       value={person.name}
-    //       id="name"
-    //       type="text"
-    //       className="form-control"
-    //     />
-    //   </div>
-    //   <div className="mb-3">
-    //     <label htmlFor="age" className="form-label">
-    //       Age
-    //     </label>
-    //     <input
-    //       onChange={(event) =>
-    //         setPerson({ ...person, age: parseInt(event.target.value) })
-    //       }
-    //       value={person.age}
-    //       id="age"
-    //       type="number"
-    //       className="form-control"
-    //     />
-    //   </div>
-    //   <button className="btn btn-primary" type="submit">
-    //     Submit
-    //   </button>
-    // </form>
-    //#endregion
-
     <form onSubmit={handleSubmit((data) => onSubmit(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
       </div>
-      {errors.name?.type === "required" && (
-        <p className="text-danger">The name is required</p>
-      )}
-      {errors.name?.type === "minLength" && (
-        <p className="text-danger">The name is 3 cracter</p>
-      )}
+      {errors.name && <p className="text-danger">{errors.name.message}</p>}
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+      </div>
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
